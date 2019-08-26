0375639e327a94bdf698e89e40df518e7f1ad12e
megmin <phoenix.raid.0906@gmail.com>
2019-08-26 22:09:10 +0900

    StepAsideUnityChan コミット
    発展課題まで完了

---
+++ Assets/ItemDestoryer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestoryer : MonoBehaviour {
+
+    private GameObject maincamera;
+
+    // Use this for initialization
+    void Start () {
+        maincamera = GameObject.Find("Main Camera");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (maincamera.transform.position.z > this.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
